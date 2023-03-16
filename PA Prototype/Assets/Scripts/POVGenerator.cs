099a85f280775b10c43d881f3957bae2983dae6e
TaraCarette <terra.the.great@gmail.com>
2023-03-16 15:33:40 +0100

    Bar can generate pixels for each ray, need to control pixel reaction

--- PA Prototype/Assets/Scripts/POVGenerator.cs
+++ PA Prototype/Assets/Scripts/POVGenerator.cs
@@ -16,16 +16,25 @@ public class POVGenerator : MonoBehaviour
     public float yPOVSize;
 
     private FieldOfView eyeScript;
+    private float pixelWidth;
+    private float pixelHeight;
 
     // Start is called before the first frame update
     void Start()
     {
+        // extract the number of rays from the variables of the eye
         eyeScript = eye.GetComponent<FieldOfView>();
+        int rays = (int) ((eyeScript.viewAngle / 10) * eyeScript.rayPer10Degree) + 1;
+
+        // calculate the size of the pixels representing
+        // the info from each raycast
+        pixelWidth = xPOVSize / rays;
+        pixelHeight = yPOVSize - 8;
 
 
 
+        // create the POV bar that will hold all the pixels
         GameObject imgObject = new GameObject("POV");
-
         RectTransform trans = imgObject.AddComponent<RectTransform>();
         trans.transform.SetParent(canvas.transform); // setting parent
         trans.localScale = Vector3.one;
@@ -34,24 +43,32 @@ public class POVGenerator : MonoBehaviour
 
         Image image = imgObject.AddComponent<Image>();
         image.raycastTarget = false; // ensure this image won't trigger the raycast
-        // image.sprite = testSprite; // assign a sprite to the image
-        image.color = Color.blue;
+        image.color = Color.blue; // might change this to something more specific to look good
 
 
 
         GameObject imgView;
         RectTransform transLoop;
         Image imageLoop;
-        for (int i = 0; i < 2; i++) 
+        float posX;
+        for (int i = 1; i <= rays; i++) 
         {
+            // creating object to hold information of one ray
             imgView = new GameObject("Ray_" + i.ToString());
 
+            // setting that object to be relative to POV bar
             transLoop = imgView.AddComponent<RectTransform>();
             transLoop.transform.SetParent(imgObject.transform); // setting parent
             transLoop.localScale = Vector3.one;
-            transLoop.anchoredPosition = new Vector2(0f, 0f); // setting position, will be on center
-            transLoop.sizeDelta= new Vector2(20, 30); // custom size
 
+            // setting size of pixel
+            transLoop.sizeDelta= new Vector2(pixelWidth, pixelHeight); // custom size
+
+            // positioning the pixel on the POV bar
+            posX = (-xPOVSize / 2) + (i * pixelWidth) - (pixelWidth / 2);
+            transLoop.anchoredPosition = new Vector2(posX, 0f); // setting position, will be on center
+
+            // creating actual image which will hold the visual information for the pixel
             imageLoop = imgView.AddComponent<Image>();
             imageLoop.raycastTarget = false; // ensure this image won't trigger the raycast
 

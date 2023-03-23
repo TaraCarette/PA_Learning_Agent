d4ce254781fbb1be2dbf75f63f0f5d8c9e38d753
TaraCarette <terra.the.great@gmail.com>
2023-03-23 21:01:02 +0100

    Sticky added but need picked up object to register collision

--- PA Prototype/Assets/Scripts/StickyAgent.cs
+++ PA Prototype/Assets/Scripts/StickyAgent.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class StickyAgent : MonoBehaviour
 {
     private bool stickyOn;
+    private bool touching;
+    private List<Transform> touchingObj;
     SpriteRenderer sr;
 
     // Start is called before the first frame update
@@ -12,23 +14,66 @@ public class StickyAgent : MonoBehaviour
     {
         stickyOn = false;
         sr = GetComponent<SpriteRenderer>();
+        touchingObj = new List<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        // when change status, change outline to indicate
+        // and update relationship to any other objects already touching as needed
         if (Input.GetKeyDown(KeyCode.S))
         {
+            Debug.Log("Changed sticky status");
             stickyOn = !stickyOn;
 
-            // colour the outline to either be bright red or match base colour
             if (stickyOn)
             {
-                sr.color = Color.red;
+                sr.color = Color.white;
+                // any touching objects should become child so moves together
+                foreach (Transform obj in touchingObj)
+                {
+                    obj.parent = transform;
+                }
             } else 
             {
                 sr.color = new Color32(0xCF, 0x4E, 0x20, 0xFF);
+                // the touching objects should no longer be children
+                foreach (Transform obj in touchingObj)
+                {
+                    obj.parent = null;
+                }
             }
         }
+
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        // only register moveable objects, aka ignore the ones in the unmoveable layer
+        if (other.gameObject.layer != 6)
+        {
+            // case where already sticky should stick immediately on collision
+            if (stickyOn)
+            {
+                other.transform.parent = transform;
+            } 
+            // update list of objects agent is touching and index
+            Transform copyOther = other.transform;
+            touchingObj.Add(copyOther);
+
+
+            Debug.Log("current list");
+            foreach (Transform obj in touchingObj)
+            {
+                Debug.Log(obj);
+            }
+        }
+
+    }
+
+    void OnCollisionExit2D(Collision2D other)
+    {
+        touchingObj.Remove(other.transform);
     }
 }

6dbdf1be5b26c3896d220ce04f26db457b20f9ee
TaraCarette <terra.the.great@gmail.com>
2023-04-21 23:57:28 +0200

    Simulator working, missing sticky and generalizability

--- PA Prototype/Assets/Scripts/Controller.cs
+++ PA Prototype/Assets/Scripts/Controller.cs
@@ -25,32 +25,64 @@ public class Controller : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (Input.GetKey(KeyCode.UpArrow))
-        {
-            rgbd.transform.Translate(new Vector3(0, speed, 0) * Time.deltaTime);
-        }
-        if (Input.GetKey(KeyCode.DownArrow))
-        {
-            rgbd.transform.Translate(new Vector3(0, -speed, 0) * Time.deltaTime);
-        }
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            rgbd.transform.Rotate(new Vector3(0, 0, rotationSpeed) * Time.deltaTime);
-        }
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            rgbd.transform.Rotate(new Vector3(0, 0, -rotationSpeed) * Time.deltaTime);
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            rgbd.transform.Translate(new Vector3(-speed, 0, 0) * Time.deltaTime);
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            rgbd.transform.Translate(new Vector3(speed, 0, 0) * Time.deltaTime);
-        }
+        // if (Input.GetKey(KeyCode.UpArrow))
+        // {
+        //     moveAgent(0);
+        // }
+        // if (Input.GetKey(KeyCode.DownArrow))
+        // {
+        //     moveAgent(1);
+        // }
+        // if (Input.GetKey(KeyCode.LeftArrow))
+        // {
+        //     rotateAgent(0);
+        // }
+        // if (Input.GetKey(KeyCode.RightArrow))
+        // {
+        //     rotateAgent(1);
+        // }
+        // if (Input.GetKey(KeyCode.A))
+        // {
+        //     moveAgent(2);
+        // }
+        // if (Input.GetKey(KeyCode.D))
+        // {
+        //     moveAgent(3);
+        // }
 
         currSpeed = Vector3.Distance(rgbd.transform.position, prevLocation);
         prevLocation = rgbd.transform.position;
     }
+
+    public void moveAgent(int dir)
+    {
+        if (dir == 0)
+        {
+            rgbd.transform.Translate(new Vector3(0, speed, 0) * Time.deltaTime);
+        }
+        else if (dir == 1)
+        {
+            rgbd.transform.Translate(new Vector3(0, -speed, 0) * Time.deltaTime);
+        }
+        else if (dir == 2)
+        {
+            rgbd.transform.Translate(new Vector3(-speed, 0, 0) * Time.deltaTime);
+        }
+        else if (dir == 3)
+        {
+            rgbd.transform.Translate(new Vector3(speed, 0, 0) * Time.deltaTime);
+        }
+    } 
+
+    public void rotateAgent(int dir)
+    {
+        if (dir == 0)
+        {
+            rgbd.transform.Rotate(new Vector3(0, 0, rotationSpeed) * Time.deltaTime);
+        }
+        if (dir == 1)
+        {
+            rgbd.transform.Rotate(new Vector3(0, 0, -rotationSpeed) * Time.deltaTime);
+        }
+    }   
 }
--- PA Prototype/Assets/Scripts/PAAgent.cs
+++ PA Prototype/Assets/Scripts/PAAgent.cs
@@ -8,30 +8,63 @@ using Unity.MLAgents.Sensors;
 public class PAAgent : Agent
 {
     public GameObject bin;
+    public GameObject eye; // give it the eye you want the POV of
 
-    private float speed;
-    private float rotationSpeed;
+    [Tooltip("Found experimentally, not sure how to calculate normally")]
+    public float maxSpeed;
+
     private Vector3 startingSpot;
 
     private Rect goalRect;
+    private FieldOfView eyeScript;
+    private int rays;
 
     public override void Initialize()
     {
-        // might move controller functionality over here since just took stuff anyways
-        speed = GetComponent<Controller>().speed;
-        rotationSpeed = GetComponent<Controller>().rotationSpeed;
-
-
         // might be best to move goal bin stuff over here as well since redoing
         goalRect = bin.GetComponent<ShapeInBin>().drawBinRect(bin.transform);
 
         startingSpot = transform.localPosition;
+
+        // assuming 1 eye, may later generalize
+        eyeScript = eye.GetComponent<FieldOfView>();
+        rays = (int) ((eyeScript.viewAngle / 10) * eyeScript.rayPer10Degree) + 1;
     }
 
     public override void CollectObservations(VectorSensor sensor)
     {
-        // need to add more later
-        sensor.AddObservation(transform.localPosition);
+        // the current speed - so can tell if running into wall vs object in front of face
+        // currently just did experiemntally, will want to calculate more formally for normalization
+        sensor.AddObservation(GetComponent<Controller>().currSpeed / maxSpeed);
+
+        // the information from the raycasts -currently assuming 1 eye
+        for (int i = 0; i < rays; i++) 
+        {
+            if (eyeScript.hits[i].collider != null)
+            {
+                // add normalized distance
+                sensor.AddObservation(eyeScript.hits[i].distance / eyeScript.viewRadius);
+                // add colour being perceived
+                // colour will be represented as a onehot vector
+                if (eyeScript.hits[i].collider.transform.tag == "blue")
+                {
+                    sensor.AddOneHotObservation(0, 2);
+                } else if (eyeScript.hits[i].collider.transform.tag == "pink")
+                {
+                    sensor.AddOneHotObservation(1, 2);
+                } else {
+                    Debug.Log("Should never get here");
+                    sensor.AddObservation(new float[2] {0f, 0f}); //will need to control size but is empty 1 hot is intent
+                }
+
+            } else {
+                // return values standing in for null
+                sensor.AddObservation(-1f);
+                sensor.AddObservation(new float[2] {0f, 0f}); //will need to control size but is empty 1 hot is intent
+            }
+        }
+
+        // will need to add if currently sticky or not
     }
 
     public override void OnActionReceived(ActionBuffers actions)
@@ -39,54 +72,38 @@ public class PAAgent : Agent
         var move = actions.DiscreteActions[0]; // fix to be 4 separate later since diagonal should be option
         var rotate = actions.DiscreteActions[1];
 
-        if (move == 0)
-        {
-            transform.Translate(new Vector3(0, speed, 0) * Time.deltaTime);
-        }
-        if (move == 1)
-        {
-            transform.Translate(new Vector3(0, -speed, 0) * Time.deltaTime);
-        }
-        if (move == 2)
-        {
-            transform.Translate(new Vector3(-speed, 0, 0) * Time.deltaTime);
-        }
-        if (move == 3)
-        {
-            transform.Translate(new Vector3(speed, 0, 0) * Time.deltaTime);
-        }
+        // make the agent move and rotate as instructed
+        GetComponent<Controller>().moveAgent(move);
+        GetComponent<Controller>().rotateAgent(rotate);
 
-
-        if (rotate == 0)
-        {
-            transform.Rotate(new Vector3(0, 0, rotationSpeed) * Time.deltaTime);
-        }
-        if (rotate == 1)
-        {
-            transform.Rotate(new Vector3(0, 0, -rotationSpeed) * Time.deltaTime);
-        }
+        // later add turning sticky on and off
 
 
 
+        // punish getting stuck unmoving on walls
+        if (GetComponent<Controller>().currSpeed < 0.005)
+        {
+            AddReward(-0.1f);
+        }
+        // later turn this into more complex curiostity based exploration
         // assigning basic reward of be in the goal thing
         if (goalRect.Contains(transform.position))
         {
-            SetReward(1f);
+            AddReward(1f);
             EndEpisode();
         } else 
         {
             // punish for not reaching goal
-            SetReward(-0.1f);
+            AddReward(-0.01f);
         }
 
     }
 
     public override void OnEpisodeBegin()
     {
-        // return to same initial spot
-        // might later want to randomize starting position more
-        transform.localPosition = startingSpot;
-        transform.rotation = new Quaternion(0f, 0f, 0f, 0f);
+        // randomize starting position and rotation
+        transform.localPosition = new Vector3(Random.Range(startingSpot[0], startingSpot[0] + 4), Random.Range(startingSpot[1] - 4f, startingSpot[1] + 3f), startingSpot[2]);
+        transform.rotation = Quaternion.Euler(0f, 0f, Random.Range(0f, 360f));
     }
 
 
